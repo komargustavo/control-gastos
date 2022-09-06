cb57bc237e5c765b4db40fb1fe68bba0b1ae3d1d
komargustavo <komargustavo@hotmail.com>
2022-09-06 12:14:00 -0300

    validación

--- src/component/NuevoPresupuesto.jsx
+++ src/component/NuevoPresupuesto.jsx
@@ -1,8 +1,36 @@
-const NuevoPresupuesto = () => {
+import { useState } from "react";
+import Mensaje from "./Mensaje"
+
+const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
+  const [mensaje, setMensaje] = useState('')
+
+  const handlePresupuesto = (e) => {
+    e.preventDefault()
+    if (!(presupuesto) || (presupuesto) < 0) {
+      setMensaje('no es un presupuesto valido')
+      return
+    } 
+      setMensaje('')
+    
+  }
+
   return (
-    <>
-    <h2>Nuevo Presupuesto</h2>
-    </>
+    <div className="contenedor-presupuesto contenedor sombra">
+      <form onSubmit={handlePresupuesto} className="formulario">
+        <div className="campo">
+          <label>Definir Presupuesto</label>
+          <input
+            className="nuevo-presupuesto"
+            type="text"
+            placeholder="Añade tu Presupuesto"
+            value={presupuesto}
+            onChange={(e)=> setPresupuesto(e.target.value)}
+          />
+        </div>
+        <input type="submit" value="Añadir" />
+        {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+      </form>
+    </div>
   );
 };
 
